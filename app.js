a60231ab6adb263d02c84629794ad8be8ad02916
yashkvr <yashincrypto5@gmail.com>
2023-12-03 20:19:47 +0530

    Database connection in auth system

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require("dotenv").config()
+require("dotenv").config();
+require('./config/database').connect();
 const express = require('express');
 
 const User = require('./model/user');
@@ -10,14 +11,14 @@ app.get("/", (req, res) => {
     res.send("<h1>Hello From Auth System!</h1>")
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     const { firstname, lastname, email, password } = req.body;
 
     if (!(email && password && firstname && lastname)) {
         res.status(400).send('All Fields are required');
     }
 
-    const existingUser = User.findOne({ email: email });
+    const existingUser = await User.findOne({ email: email });
 
     if (existingUser) {
         res.status(401).send('User Already Exists');
