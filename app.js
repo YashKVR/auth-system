ab3d100b6730d64136ff26a6afe297a5e92ab8ca
yashkvr <yashincrypto5@gmail.com>
2023-12-03 21:52:41 +0530

    What is JWT and creating a token

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require('./config/database').connect();
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('./model/user');
 
@@ -13,26 +14,48 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-    const { firstname, lastname, email, password } = req.body;
+    try {
+        const { firstname, lastname, email, password } = req.body;
 
-    if (!(email && password && firstname && lastname)) {
-        res.status(400).send('All Fields are required');
+        if (!(email && password && firstname && lastname)) {
+            res.status(400).send('All Fields are required');
+        }
+
+        const existingUser = await User.findOne({ email: email });
+
+        if (existingUser) {
+            res.status(401).send('User Already Exists');
+        }
+
+        const myEncPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            password: myEncPassword
+        });
+
+        //token creation
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: '2h'
+            }
+        )
+
+        user.token = token;
+
+        //update or not in DB: TODO?
+
+        //Handle one more password situation : TODO
+
+
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error);
     }
-
-    const existingUser = await User.findOne({ email: email });
-
-    if (existingUser) {
-        res.status(401).send('User Already Exists');
-    }
-
-    const myEncPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-        firstname,
-        lastname,
-        email: email.toLowerCase(),
-        password: myEncPassword
-    })
 
 })
 
