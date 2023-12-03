d6d5fa8591650a41dac410539860e4f0a2324d1c
yashkvr <yashincrypto5@gmail.com>
2023-12-03 20:00:34 +0530

    Registering a user in auth system

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 require("dotenv").config()
 const express = require('express');
 
+const User = require('./model/user');
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello From Auth System!</h1>")
 });
+
+app.post("/register", (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!(email && password && firstname && lastname)) {
+        res.status(400).send('All Fields are required');
+    }
+
+    const existingUser = User.findOne({ email: email });
+
+    if (existingUser) {
+        res.status(401).send('User Already Exists');
+    }
+
+})
 
 
 module.exports = app
